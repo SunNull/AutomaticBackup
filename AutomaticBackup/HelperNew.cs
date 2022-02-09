940558843d568e252286a59bb58fcda5386355cb
LiSai <1747070238@qq.com>
2022-02-09 15:40:50 +0800

    1122

--- AutomaticBackup/HelperNew.cs
+++ AutomaticBackup/HelperNew.cs
@@ -24,6 +24,7 @@
                             }
                             catch (Exception)
                             {
+
                             }
                         }
                     }
